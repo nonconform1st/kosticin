b8b7c6e04eb830c2289900ed0a7df50d71bce556
nonconform1st <b000gdan@ya.ru>
2018-09-29 22:50:46 +0300

    Adaptive who block

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 $(()=>{
+    console.log(screen.width);
+
     $('.who__learn-more').click(toggleDetails);
 });
 
 function toggleDetails() {
     var $btn = $(this);
 
-    $btn.toggleClass('opened');
-    $btn.closest('.who')
+    if (screen.width <= 768) {
+        $btn.closest('.who')
+            .find('.who__details-wrapper')
+            .slideUp(200);
+    }
+    $btn.toggleClass('opened')
+        .closest('.who')
         .find('.who__details')
         .eq( $btn.closest('.who__person').index() )
         .find('.who__details-wrapper')
         .slideToggle(200);
+
 }
