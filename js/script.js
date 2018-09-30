6e8e0b5d0528c85d7f9b11aa757dea45c40f23be
nonconform1st <b000gdan@ya.ru>
2018-09-30 16:27:11 +0300

    Services Block

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 $(()=>{
-    console.log(screen.width);
-
     $('.who__learn-more').click(toggleDetails);
 });
 
@@ -8,15 +6,28 @@ function toggleDetails() {
     var $btn = $(this);
 
     if (screen.width <= 768) {
-        $btn.closest('.who')
+        $btn.toggleClass('opened')
+            .closest('.who')
+            .find('.who__details')
+            .eq( $btn.closest('.who__person').index() )
             .find('.who__details-wrapper')
-            .slideUp(200);
+            .slideToggle(200, ()=>{
+                $btn.closest('.who')
+                    .find('.who__details')
+                    .eq( $btn.closest('.who__person').index() )
+                    .siblings()
+                    .find('.who__details-wrapper')
+                    .slideUp(100)
+            })
+
     }
-    $btn.toggleClass('opened')
-        .closest('.who')
-        .find('.who__details')
-        .eq( $btn.closest('.who__person').index() )
-        .find('.who__details-wrapper')
-        .slideToggle(200);
+    else {
+        $btn.toggleClass('opened')
+            .closest('.who')
+            .find('.who__details')
+            .eq( $btn.closest('.who__person').index() )
+            .find('.who__details-wrapper')
+            .slideToggle(200);
+    }
 
 }
