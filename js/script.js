a6794470a1e6bc409ae8166351975d9e2d16094f
nonconform1st <b000gdan@ya.ru>
2018-10-01 20:20:50 +0300

    Ancor aniation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(()=>{
     $('.who__learn-more').click(toggleDetails);
+
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
 });
 
 function toggleDetails() {
